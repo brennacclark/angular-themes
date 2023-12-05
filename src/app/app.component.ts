e1b68382ca4a8123e2be0248b503316792773bf2
Brenna Clark <brennacclark@gmail.com>
2023-12-04 19:33:18 -0500

    Adding Basic Theme Switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-themes';
+export class AppComponent implements OnInit {
+  title = 'Angular Themes';
+
+  // storedTheme = "theme-light";
+  storedTheme: string = localStorage.getItem('theme-color')!;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  setTheme(theme: string) {
+    // this.storedTheme = theme;
+    localStorage.setItem('theme-color', theme);
+    this.storedTheme = localStorage.getItem('theme-color')!;
+  }
 }
